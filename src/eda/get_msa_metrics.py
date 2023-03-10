0d4b970995144bff2ff0199946721a4524517eea
Patrick Darren Bryant <pbryant@llama.imp.fu-berlin.de>
2023-03-10 17:28:30 +0100

    Neff

--- src/eda/get_msa_metrics.py
+++ src/eda/get_msa_metrics.py
@@ -2,7 +2,7 @@ import argparse
 import sys
 import numpy as np
 import pandas as pd
-sys.path.insert('../msa/', 0)
+sys.path.insert(0, '../msa/')
 import pdb
 #Custom
 from pair_msas import read_a3m, pair_msas, analyse_paired_msa
@@ -23,20 +23,27 @@ args = parser.parse_args()
 selected_ints = pd.read_csv(args.selected_ints[0])
 msa_dir = args.msa_dir[0]
 
+#Go through all ints and get the metrics
+all_neffs, all_gaps = [], []
 for ind, row in selected_ints.iterrows():
-    pdb.set_trace()
-#Read MSAS
-msa1='../../data/dev/4G4S_O.a3m'
-msa2='../../data/dev/4G4S_P.a3m'
-msa1, ox1 = read_a3m(msa1)
-msa2, ox2 = read_a3m(msa2)
-#Get the unique ox seqs from the MSAs
-u_ox1, inds1 = np.unique(ox1, return_index=True)
-u_msa1 = msa1[inds1]
-u_ox2, inds2 = np.unique(ox2, return_index=True)
-u_msa2 = msa2[inds2]
-#Pair MSAs
-paired_msa = pair_msas(u_ox1, u_ox2, u_msa1, u_msa2)
-#Analyse
-Neff, gap_fraction = analyse_paired_msa(paired_msa)
+    print(ind,'out of',len(selected_ints))
+    id1, id2 = row.complex_id.split('-')
+    #Read MSAs
+    msa1, ox1 = read_a3m(msa_dir+id1+'.a3m')
+    msa2, ox2 = read_a3m(msa_dir+id2+'.a3m')
+    #Get the unique ox seqs from the MSAs
+    u_ox1, inds1 = np.unique(ox1, return_index=True)
+    u_msa1 = msa1[inds1]
+    u_ox2, inds2 = np.unique(ox2, return_index=True)
+    u_msa2 = msa2[inds2]
+    #Pair MSAs
+    paired_msa = pair_msas(u_ox1, u_ox2, u_msa1, u_msa2)
+    #Analyse
+    Neff, gap_fraction = analyse_paired_msa(paired_msa)
+    all_neffs.append(Neff)
+    all_gaps.append(gap_fraction)
+
+#Save
 pdb.set_trace()
+selected_ints['Neff']=all_neffs
+selected_ints['Gap_fraction']=all_gaps
