73b6b9ef4ca7997d2d7a57e0a5c59ce2a21ff998
Patrick Bryant <patrick.bryant@live.com>
2023-06-04 20:55:24 +0200

    acc

--- src/analysis/vis.py
+++ src/analysis/vis.py
@@ -7,39 +7,79 @@ from sklearn import metrics
 import pdb
 pos_scores = pd.read_csv('../../data/dev/blackbox_cracking/all_pos_scores.csv', header=None)
 neg_scores = pd.read_csv('../../data/dev/blackbox_cracking/all_neg_scores.csv', header=None)
+outdir='../../data/plots/'
 #Check overlap:
 print('Overlap:', neg_scores[neg_scores[0].isin(pos_scores[0])])
-#plt.violinplot([pos_scores[3], neg_scores[3]])
-#plt.show()
 print('Positive set:')
 print(pos_scores.describe())
 print('Negative set:')
 print(neg_scores.describe())
 
+#PR curve
 y_true = np.zeros(len(pos_scores)+len(neg_scores))
 y_true[:len(pos_scores)]=1
 probas_pred = np.concatenate([pos_scores[3], neg_scores[3]])
 precision, recall, thresholds = precision_recall_curve(y_true, probas_pred)
 AUC = metrics.auc(recall, precision)
 print('PR AUC:', AUC)
-# plt.plot(recall, precision, label='AUC:'+str(np.round(AUC,3)))
-# plt.legend()
-# plt.xlabel('Recall')
-# plt.ylabel('Precision')
-# plt.ylim([0.5,1])
-# plt.show()
-#
-# #ROC
-# fpr, tpr, thresholds = metrics.roc_curve(y_true, probas_pred)
-# AUC = metrics.auc(fpr, tpr)
-# plt.plot(fpr, tpr, label='AUC:'+str(np.round(AUC,3)))
-# plt.legend()
-# plt.xlabel('FPR')
-# plt.ylabel('TPR')
-# plt.show()
+fig, ax = plt.subplots(figsize=(9/2.54, 9/2.54))
+plt.plot(recall, precision, label='AUC:'+str(np.round(AUC,3)))
+plt.legend()
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.ylim([0.5,1])
+plt.tight_layout()
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+plt.savefig(outdir+'pr_curve.png', format='png', dpi=300)
+plt.close()
+
+#ROC curve
+fpr, tpr, thresholds = metrics.roc_curve(y_true, probas_pred)
+AUC = metrics.auc(fpr, tpr)
+fig, ax = plt.subplots(figsize=(9/2.54, 9/2.54))
+plt.plot(fpr, tpr, label='AUC:'+str(np.round(AUC,3)))
+plt.legend()
+plt.xlabel('FPR')
+plt.ylabel('TPR')
+plt.tight_layout()
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+plt.savefig(outdir+'roc_curve.png', format='png', dpi=300)
+plt.close()
 
 #y pred
+x, acc = [], []
 for t in np.arange(0,1,0.01):
     y_pred = np.zeros(len(pos_scores)+len(neg_scores))
     y_pred[probas_pred>=t]=1
-    print('Accuracy:',t, metrics.accuracy_score(y_true, y_pred))
+    #print('Accuracy:',t, metrics.accuracy_score(y_true, y_pred))
+    x.append(t)
+    acc.append(metrics.accuracy_score(y_true, y_pred))
+
+fig, ax = plt.subplots(figsize=(9/2.54, 9/2.54))
+plt.plot(x, acc)
+plt.xlabel('pDockQ threshold')
+plt.ylabel('Accuracy')
+plt.tight_layout()
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+plt.savefig(outdir+'acc_vs_pdockqs.png', format='png', dpi=300)
+plt.close()
+
+#Plot the comparison methods
+accuracies = [0.50, 0.50, 0.51, 0.51, 0.52, 0.63]
+labels = [ 'Richoux-LSTM', 'DeepFE', 'Richoux-FC', 'SPRINT', 'PIPR', 'FoldDock']
+fig, ax = plt.subplots(figsize=(9/2.54, 9/2.54))
+plt.scatter(range(len(accuracies)), accuracies)
+plt.xticks(ticks=range(len(accuracies)), labels=labels, rotation=45)
+plt.axhline(y=0.5, xmin=0, xmax=5, linestyle='--', color='grey', label='random')
+plt.ylabel('Accuracy')
+plt.title('Accuracies')
+plt.ylim([0.48,1])
+plt.legend()
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+plt.tight_layout()
+plt.savefig(outdir+'accuracies.png', format='png', dpi=300)
+plt.close()
